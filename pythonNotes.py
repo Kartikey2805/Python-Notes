0c2e606e4607890c9f72dae3a6097fd80546ebc0
Kartikey Goel <kartikeygoel093@gmail.com>
2023-03-19 22:05:51 +0530

    modified

--- pythonNotes.py
+++ pythonNotes.py
@@ -191,6 +191,7 @@ f.close()
 # os.rmdir("myfolder")
 
 # automatically closes a file stream
+# a+ means append + read mode 
 
 with open('data.txt','a+') as file:
     file.write('\nI am also good')
