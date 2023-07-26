3a5f9215e1a4d6b9624346e21744cea2219a5d71
Kartikey Goel <kartikeygoel093@gmail.com>
2023-07-27 00:18:16 +0530

    udemy

---
+++ Udemy Notes.py
@@ -0,0 +1,17 @@
+# Start a particular version of python by following commands
+
+# py --list (all versions installed)
+# py --list-paths
+# py --version
+# py -3.9 (start a session using particular python version)
+# py - will use default verison of python
+
+Floats do not have exact representation 
+like 0.1 is represented as 0.9999999....
+format(0.1,'.25f') 
+# 0.1 + 0.1 == 0.2 // output is False
+
+datatypes are objects
+they have state and functionality
+int is an object, uses __add__ to add a number
+
